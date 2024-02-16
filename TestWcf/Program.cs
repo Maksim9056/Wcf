5303a105fef6f6c6a3fcd0068731de8371e123d9
Maks <bobreczovm@inbox.ru>
2024-02-16 13:52:59 +0300

    Добавьте файлы проекта.

---
+++ TestWcf/Program.cs
@@ -0,0 +1,15 @@
+﻿using WcfServiceLibrary1;
+
+namespace TestWcf
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Service1();
+            var result=   client.Add(10, 80);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
